24b31bece29237f73bdae2b59ed45bd8a3989eb4
dmitrikurtukov <kurtukov9@gmail.com>
2025-01-17 18:41:23 +0200

    Changing Icon to Box component to resolve issues

--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -10,7 +10,7 @@ import { MdPhoneIphone } from "react-icons/md";
 import { SiNintendo } from "react-icons/si";
 import { BsGlobe } from "react-icons/bs";
 import { Platform } from "@/hooks/useGames";
-import { HStack, Icon } from "@chakra-ui/react";
+import { HStack, Box } from "@chakra-ui/react";
 import { IconType } from "react-icons";
 
 interface Props {
@@ -35,9 +35,12 @@ function PlatformIconList({ platforms }: Props) {
       {platforms.map((platform) => {
         const IconComponent = iconMap[platform.slug];
         return (
-          <Icon key={platform.id} color="gray.500">
-            <IconComponent />
-          </Icon>
+          <Box
+            key={platform.id}
+            as={IconComponent}
+            color="gray.500"
+            fontSize={17}
+          />
         );
       })}
     </HStack>
